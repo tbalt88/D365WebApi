0ecb9cc1ddf23ad59e0619efbd6e9955f60730f6
mariusso <marius.solend@gmail.com>
2016-06-13 17:51:39 +0200

    Added to Git

---
+++ WebAPI.REST.js/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("WebAPI.REST.js")]
+[assembly: AssemblyDescription("Javascript library for the Dynamics CRM Web API")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Acando")]
+[assembly: AssemblyProduct("WebAPI.REST.js")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("17d3edb4-ece3-402c-a4c3-86f83b8c1283")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
