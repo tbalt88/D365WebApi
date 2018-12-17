f64852048fb386614d3e12c6b77357ad8df9946d
mariusso <marius.solend@gmail.com>
2018-12-17 17:02:13 +0100

    Changes to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,14 @@
+let path = require("path");
+
 module.exports = {
     //devtool: 'source-map',
-    entry: "./source/D365WebApi.js",
+    entry: {
+        "D365WebApi": "./source/D365WebApi.js"
+    },
     mode: "production",
     output: {
-        filename: "D365WebApi.js",
-        path: require("path").resolve(__dirname, "output"),
+        filename: "[name].js",
+        path: path.resolve(__dirname, "output"),
         library: "D365WebApi",
         libraryTarget:"var"
     },
