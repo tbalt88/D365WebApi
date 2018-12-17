9ab8956cc3b3fbfe21ca13d757056eaced6c6c83
mariusso <marius.solend@gmail.com>
2018-12-17 17:23:17 +0100

    Minor changes

--- source/D365WebApi.js
+++ source/D365WebApi.js
@@ -1,18 +1,4 @@
-﻿// =========================================================================
-// WebAPI.REST.js
-//
-// Library for Dynamics CRM Web API inspired by SDK.REST.js
-// Can be used in both form scripts and web resources
-// Uses superagent.js for requests and q.js for promises
-// 
-// Change _webAPIPath and _defaultHeaders as necessary for newer versions
-//
-// Web API reference: https://msdn.microsoft.com/en-us/library/mt593051.aspx
-//
-// Developed by Marius Solend, Acando
-// =========================================================================
-
-let superagent = require("superagent");
+﻿let superagent = require("superagent");
 let Q = require("q");
 
 export function context() {
